ce9d144c2863650c06e29fbaea3b45b19a3fd89f
Sohaib Aslam <seboodogar@gmail.com>
2025-06-24 13:33:47 +0500

    Server is Created and connected to DB

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+});
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
