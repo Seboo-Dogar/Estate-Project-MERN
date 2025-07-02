c3f26812c838601fbda804727906c824933e8751
Sohaib Aslam <seboodogar@gmail.com>
2025-07-02 07:19:03 +0500

    Just create API routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoute from './routes/userRoute.js';
 
 dotenv.config();
 
@@ -19,3 +20,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/api/user', userRoute);
